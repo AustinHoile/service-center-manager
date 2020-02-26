ab967652421d6779f9e0d99e5f9b3a869692490d
Austin Hoile <61429802+AustinHoile@users.noreply.github.com>
2020-02-25 18:07:31 -0600

    Initial Commit

---
+++ ServiceCenterManager/ServiceCenterManager/ServiceCenterManager/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ServiceCenterManager
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_login));
+            this.lbl_userName = new System.Windows.Forms.Label();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.lbl_loginUnsuccessful = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_userName
+            // 
+            this.lbl_userName.AutoSize = true;
+            this.lbl_userName.Location = new System.Drawing.Point(21, 38);
+            this.lbl_userName.Name = "lbl_userName";
+            this.lbl_userName.Size = new System.Drawing.Size(83, 20);
+            this.lbl_userName.TabIndex = 0;
+            this.lbl_userName.Text = "Username";
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(21, 80);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(78, 20);
+            this.lbl_password.TabIndex = 1;
+            this.lbl_password.Text = "Password";
+            // 
+            // txt_username
+            // 
+            this.txt_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_username.Location = new System.Drawing.Point(105, 38);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(127, 26);
+            this.txt_username.TabIndex = 2;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(105, 80);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(127, 26);
+            this.txt_password.TabIndex = 3;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.Location = new System.Drawing.Point(31, 129);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(73, 27);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbl_userName);
+            this.groupBox1.Controls.Add(this.btn_login);
+            this.groupBox1.Controls.Add(this.txt_password);
+            this.groupBox1.Controls.Add(this.txt_username);
+            this.groupBox1.Controls.Add(this.lbl_password);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(317, 121);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(249, 184);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Please Login";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ServiceCenterManager.Properties.Resources._588a668cd06f6719692a2d1b;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(239, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lbl_title.Location = new System.Drawing.Point(150, 45);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(330, 25);
+            this.lbl_title.TabIndex = 7;
+            this.lbl_title.Text = "Hoile Motors Service Manager";
+            // 
+            // lbl_loginUnsuccessful
+            // 
+            this.lbl_loginUnsuccessful.AutoSize = true;
+            this.lbl_loginUnsuccessful.Location = new System.Drawing.Point(419, 105);
+            this.lbl_loginUnsuccessful.Name = "lbl_loginUnsuccessful";
+            this.lbl_loginUnsuccessful.Size = new System.Drawing.Size(0, 13);
+            this.lbl_loginUnsuccessful.TabIndex = 8;
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(606, 450);
+            this.Controls.Add(this.lbl_loginUnsuccessful);
+            this.Controls.Add(this.lbl_title);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.frm_login_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_userName;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.Label lbl_loginUnsuccessful;
+    }
+}
+
---
+++ ServiceCenterManager/ServiceCenterManager/ServiceCenterManager/Form1.cs
@@ -0,0 +1,54 @@
+﻿/*
+Austin Hoile
+IS318 Final Project
+Service Manager Version 1.0
+11/25/2018
+ */
+using System;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace ServiceCenterManager
+{
+    public partial class frm_login : Form
+    {
+        private OleDbConnection connection = new OleDbConnection();
+        public frm_login()
+        {
+            InitializeComponent();
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\EmployeeInfo.mdb; 
+                                            Persist Security Info=False;"; //Connection to the Access database with a relative path 
+        }
+        private void frm_login_Load(object sender, EventArgs e)
+        {
+        }
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = connection;
+            command.CommandText = "select * from Logins where Username = '" + txt_username.Text + "' and Password = '" + txt_password.Text + "'"; //Pulls the username and password from the "Logins" table with the users' login information. 
+            OleDbDataReader reader = command.ExecuteReader();
+
+            int count = 0;
+            while (reader.Read()) //If the counter counts to 1, the login info matches and the service manager window will open.
+            {
+                count = count + 1;
+            }
+            if (count == 1)
+            {
+                connection.Close();
+                connection.Dispose();
+                this.Hide();
+                ServiceManager sm = new ServiceManager();
+                sm.ShowDialog();
+            }
+            else
+            {
+                lbl_loginUnsuccessful.Text = "Login Unsuccessful";
+            }
+
+            connection.Close();
+        }
+    }
+}
